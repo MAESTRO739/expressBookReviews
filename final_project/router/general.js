22e4700dbf3e0ea7896c9aa68d2fa78d315276ec
Lasch739 <butakova.tatyana79@gmail.com>
2024-06-29 17:02:45 -0400

    completed 3 tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,25 +11,33 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/isbn/:isbn', function (req, res) {
+  const ISBN = req.params.isbn;
+  if (books[ISBN]) {
+    return res.send(JSON.stringify(books[ISBN], null, 4));
+  } else {
+      return res.status(404).send(`Book with ISBN ${ISBN} not found.`);
+  }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const matchingBooks = Object.values(books).filter(book => book.author === author);
+  if (matchingBooks.length > 0) {
+    return res.send(JSON.stringify(matchingBooks, null, 4));
+  } else {
+    return res.status(404).send(`No books found by author ${author}`);
+  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
