39a35c705fd5ef082b4a8833a25d44ec666905ca
MAESTRO739 <butakova.tatyana79@gmail.com>
2024-09-02 22:48:49 -0400

    completed tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,44 +23,65 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get('/', async function (req, res) {
   try {
-    const response = await axios.get('http://localhost:5005/books');
+    const response = await axios.get('http://localhost:5000/books');
     const books = response.data;
-    return res.status(200).json(books);
-  } catch (error) {
-    console.error(error);
+    return res.send(JSON.stringify(books, null, 4));
+  } catch {
     return res.status(500).json({message: "Error getting book list"});
   }
 });
 
+public_users.get('/books', function (req, res) {
+  return res.send(JSON.stringify(books, null, 4));
+});
+
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const ISBN = req.params.isbn;
-  if (books[ISBN]) {
-    return res.send(JSON.stringify(books[ISBN], null, 4));
-  } else {
-      return res.status(404).send(`Book with ISBN ${ISBN} not found.`);
+  try {
+    const response = await axios.get('http://localhost:5000/books');
+    const books = response.data;
+    if (books[ISBN]) {
+      return res.send(JSON.stringify(books[ISBN], null, 4));
+    } else {
+        return res.status(404).send(`Book with ISBN ${ISBN} not found.`);
+    }
+  } catch {
+    return res.status(500).json({message: "Error getting book details by ISBN"});
   }
- });
+});
   
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author;
-  const matchingBooks = Object.values(books).filter(book => book.author === author);
-  if (matchingBooks.length > 0) {
-    return res.send(JSON.stringify(matchingBooks, null, 4));
-  } else {
-      return res.status(404).send(`No books found by author ${author}`);
+  try {
+    const response = await axios.get('http://localhost:5000/books');
+    const books = response.data;
+    const matchingBooks = Object.values(books).filter(book => book.author === author);
+    if (matchingBooks.length > 0) {
+      return res.send(JSON.stringify(matchingBooks, null, 4));
+    } else {
+        return res.status(404).send(`No books found by author ${author}`);
+    }
+  } catch {
+    return res.status(500).json({message: "Error getting book details by author"});
   }
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   const title = req.params.title;
-  const matchingBook = Object.values(books).filter(book => book.title === title);
-  if (matchingBook.length > 0) {
-    return res.send(JSON.stringify(matchingBook, null, 4));
-  } else {
-    return res.status(404).send(`No book found under title ${title}`);
+  try {
+    const response = await axios.get('http://localhost:5000/books');
+    const books = response.data;
+    const matchingBook = Object.values(books).filter(book => book.title === title);
+    if (matchingBook.length > 0) {
+      return res.send(JSON.stringify(matchingBook, null, 4));
+    } else {
+      return res.status(404).send(`No book found under title ${title}`);
+    }
+  } catch {
+    return res.status(500).json({message: "Error getting book details by title"});
   }
 });
 
